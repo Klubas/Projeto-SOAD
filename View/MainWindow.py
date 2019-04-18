14b299297bd1d245b22267d7279770e59f62f44b
lucas.kluber <lucas.kluber@gmail.com>
2019-04-17 23:06:39 -0300

    Initial commit

---
+++ View/MainWindow.py
@@ -0,0 +1,4 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MainWindow(object):
+    def setupUi(self, Dialog):
---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('..')
+
+from Controller.AppWindow import AppWindow
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = AppWindow()
+    sys.exit(app.exec_())
