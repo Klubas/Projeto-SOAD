1b36d95c612cd0d935872b32dfc1e56dbe65d9d0
lucas.kluber <lucas.kluber@gmail.com>
2019-04-18 00:05:19 -0300

    Pycharm

--- Controller/AppWindow.py
+++ Controller/AppWindow.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.append('..')
 
-import PyQt5
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QAbstractItemView, QApplication, QDialog,
                              QLineEdit, QPushButton, QTableWidget,
                              QTableWidgetItem, QWidget)
 
-class AppWindow(QDialog, Ui_Dialog):
+class AppWindow(QDialog, MainWindow):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        pass
--- View/MainWindow.py
+++ View/MainWindow.py
@@ -1,4 +1,7 @@
+import sys
+sys.path.append('..')
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class MainWindow(object):
     def setupUi(self, Dialog):
+        pass
