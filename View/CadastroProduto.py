7ccb63c891a406250e5bbc685fa617b16095662e
lucas kluber <lucas.kluber@gmail.com>
2019-04-21 02:16:14 -0300

    barra de menus

---
+++ View/CadastroProduto.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Resources/CadastroProduto.ui',
+# licensing of 'Resources/CadastroProduto.ui' applies.
+#
+# Created: Sun Apr 21 02:15:08 2019
+#      by: pyside2-uic  running on PySide2 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_CadastroPessoa(object):
+    def setupUi(self, CadastroPessoa):
+        CadastroPessoa.setObjectName("CadastroPessoa")
+        CadastroPessoa.resize(576, 390)
+
+        self.retranslateUi(CadastroPessoa)
+        QtCore.QMetaObject.connectSlotsByName(CadastroPessoa)
+
+    def retranslateUi(self, CadastroPessoa):
+        CadastroPessoa.setWindowTitle(QtWidgets.QApplication.translate("CadastroPessoa", "Cadastro de Produto", None, -1))
+
